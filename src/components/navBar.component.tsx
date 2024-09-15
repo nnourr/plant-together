8c17025bfc90c310408a65950963a1a57222f3c0
nnourr <nourqwerty123@gmail.com>
2024-09-15 17:14:04 -0400

    navigate home from navbar properly

--- src/components/navBar.component.tsx
+++ src/components/navBar.component.tsx
@@ -1,14 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
 export const NavBar: React.FC = () => {
+  const navigate = useNavigate();
   return (
     <div className="bg-slate-900 w-full h-16 flex items-center px-8 justify-between">
-      <a href="/">
+      <a href="/" onClick={() => navigate("/")}>
         <h1 className="text-white font-mono text-xl font-bold">
           Plant Together
         </h1>
       </a>
       <button
         className="text-white font-bold border-white/20 border-2 rounded-xl px-2 py-1 transition-all hover:border-white/60"
-        onClick={() => (window.location.pathname = "")}
+        onClick={() => navigate("/")}
       >
         new room
       </button>
