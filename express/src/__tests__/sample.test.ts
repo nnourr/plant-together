48d86a46bf3eebff752d51ed67d0ce30fbebfe59
Jason Tan <jaskyle4@gmail.com>
2025-02-10 11:45:46 -0500

    fix: jest testing framework

--- express/src/__tests__/sample.test.ts
+++ express/src/__tests__/sample.test.ts
@@ -1,13 +1,71 @@
-describe('Sample Test', () => {
-  beforeEach(() => {
-    jest.clearAllMocks()
+import * as room from "../room/room.repo.js";
+import { jest } from "@jest/globals"
+import sql from "../database/database.js";
+
+describe('Repositories', () => {
+  beforeAll(async () => {
   })
 
-  afterEach(() => {
-    jest.resetAllMocks()
-  })
-
-  it('should test that true === true', () => {
+  it('does', () => {
     expect(true).toBe(true)
   })
+
+  describe("Rooms Repository", () => {
+    const defaultRoomId = '1'
+    const defaultRoomName = 'Room Name Default'
+    const defaultDocumentName = 'Default Document Name'
+
+    beforeEach(async () => {
+      // each test has at least one room and one document
+      await room.createRoomWithDocument(defaultRoomId, defaultRoomName, defaultDocumentName)
+    })
+
+    afterEach(async () => {
+      // undo actions that occurred with the test
+      await sql!`TRUNCATE room, document RESTART IDENTITY CASCADE`;
+    })
+
+    afterAll(async () => {
+      // Clean up after each test
+      await sql.end()
+    });
+
+    it('creates room with 1 document', async () => {
+      const roomId = '100'
+      const roomName = 'Room 100'
+      const documentName = 'Document One'
+
+      await room.createRoomWithDocument(roomId, roomName, documentName)
+      const roomWithDocuments = await room.getRoomWithDocuments(roomId)
+
+      expect(roomWithDocuments?.room_id).toBe(roomId)
+      expect(roomWithDocuments?.documents).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({ name: documentName })
+        ])
+      )
+    })
+
+    it('adds 1 document to a room', async () => {
+      const documentName = "Document Two"
+
+      await room.createDocumentInRoom(defaultRoomId, documentName)
+      const roomWithDocuments = await room.getRoomWithDocuments(defaultRoomId)
+
+      expect(roomWithDocuments?.room_id).toBe(defaultRoomId)
+      expect(roomWithDocuments?.documents).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({ name: documentName })
+        ])
+      )
+    })
+
+    it('gets documents from a room', async () => {
+      const roomWithDocuments = await room.getRoomWithDocuments(defaultRoomId)
+
+      expect(roomWithDocuments?.room_id).toBe(defaultRoomId)
+      expect(roomWithDocuments?.documents).toBeInstanceOf(Array)
+    })
+  })
+
 })
--- express/src/database/database.ts
+++ express/src/database/database.ts
@@ -1,16 +1,49 @@
 import { DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER } from "../config.js";
-import postgres from 'postgres'
+import postgres, { PostgresType, type Sql } from 'postgres'
 import prexit from 'prexit'
+import { PostgresMock } from "pgmock";
 
-const sql = postgres({
-  user: DB_USER,
-  pass: DB_PASS,
-  host: DB_HOST,
-  database: DB_NAME, 
-  port: (DB_PORT || 5432) as number,     
-  idle_timeout: 20,
-  max_lifetime: 60 * 30            
-});
+interface SqlWithMock extends Sql {
+  mock?: PostgresMock;
+}
+
+// database.ts
+let sql: SqlWithMock = {} as SqlWithMock;
+
+
+if (process.env.NODE_ENV === 'test') {
+  const mock = await PostgresMock.create()
+  const connectionString = await mock.listen(6969)
+  sql = postgres(connectionString)
+  sql.mock = mock
+
+  // TODO: Replace with db migrations
+  await sql`
+    CREATE TABLE IF NOT EXISTS room (
+      id SERIAL PRIMARY KEY,
+      name TEXT NOT NULL
+    );
+  `
+  // TODO: Replace with db migrations
+  await sql`
+    CREATE TABLE IF NOT EXISTS document (
+      id SERIAL PRIMARY KEY,
+      name TEXT NOT NULL,
+      room_id INTEGER NOT NULL REFERENCES room(id)
+    );
+  `
+
+} else {
+  sql = postgres({
+    user: DB_USER,
+    pass: DB_PASS,
+    host: DB_HOST,
+    database: DB_NAME, 
+    port: (DB_PORT || 5432) as number,     
+    idle_timeout: 20,
+    max_lifetime: 60 * 30,         
+  });
+}
 
 // Test the connection
 const alive = await sql`SELECT NOW()`
