65c4f467e759d5c7089c1fb7070de94e28942261
Bcutch <bcutch13@gmail.com>
2025-02-12 23:20:27 -0500

    tested component

--- react/src/components/inputField.component.tsx
+++ react/src/components/inputField.component.tsx
@@ -2,12 +2,13 @@ interface InputFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
     title?: string;
 }
 
-export const InputField: React.FC<InputFieldProps> = ({ title, ...props }) => {
+export const InputField: React.FC<InputFieldProps> = ({ title, id = "input-field", ...props }) => {
 
   return (
     <div className="flex gap-4 flex-col md:flex-row box-border mt-8">
-      <label htmlFor="input-field">{title}</label>
+      <label htmlFor={id}>{title}</label>
       <input {...props}
+        id = {id}
         className="rounded-xl bg-transparent border-2 border-white/20 text-2xl w-[80vw] lg:w-auto px-4 py-2"
       />
     </div>
