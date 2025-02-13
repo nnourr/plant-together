f368d6b7844a323beabd094f3ee8a7a391f89162
Brettan Cutchall <bcutchal@uoguelph.ca>
2025-02-12 23:32:37 -0500

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: Noureldeen Ahmed <nourelde@uoguelph.ca>

--- react/src/components/inputField.component.tsx
+++ react/src/components/inputField.component.tsx
@@ -6,7 +6,7 @@ export const InputField: React.FC<InputFieldProps> = ({ title, id = "input-field
 
   return (
     <div className="flex gap-4 flex-col md:flex-row box-border mt-8">
-      <label htmlFor={id}>{title}</label>
+      {title && <label htmlFor={id}>{title}</label>}
       <input {...props}
         id = {id}
         className="rounded-xl bg-transparent border-2 border-white/20 text-2xl w-[80vw] lg:w-auto px-4 py-2"
