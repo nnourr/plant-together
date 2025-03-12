66bbe1bb7e70ce31d31d0e30ca313faabf048f2e
nnourr <nourqwerty123@gmail.com>
2025-03-11 23:44:12 -0400

    redis setup

--- express/src/config.ts
+++ express/src/config.ts
@@ -12,3 +12,6 @@ export const DB_NAME = process.env.DB_NAME;
 export const DB_PORT = process.env.DB_PORT;
 export const DB_PASS = process.env.DB_PASS;
 export const DB_USER = process.env.DB_USER;
+
+// Redis
+export const REDIS_URL = process.env.REDIS_URL;
---
+++ express/src/redis/redis.ts
@@ -0,0 +1,13 @@
+import { createClient } from "redis";
+import { logger } from "../logger.js";
+import { REDIS_URL } from "../config.js";
+
+const redisClient = createClient({
+  url: REDIS_URL,
+});
+
+redisClient.on("error", (err) => logger.error("Redis Client Error", err));
+
+await redisClient.connect();
+
+export default redisClient;
