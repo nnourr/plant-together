1be4cc5a168f5ee2166ecd01d3d84ab1c213cd20
nnourr <nourqwerty123@gmail.com>
2025-03-18 14:56:57 -0400

    simple and easy redis mock

--- express/src/__tests__/__mocks__/redis.mock.ts
+++ express/src/__tests__/__mocks__/redis.mock.ts
@@ -1,13 +1,6 @@
 import { jest } from "@jest/globals";
 
 export class mockRedis {
-  private static store: Record<string, any[]> = {};
-
-  static flushAll = jest.fn();
-
-  static xAdd = jest.fn();
-
-  static xRead = jest.fn();
-
+  static xRead = jest.fn<() => Promise<any>>();
   static commandOptions = jest.fn();
 }
