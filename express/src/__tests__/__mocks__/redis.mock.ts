0dcb82298aafb985e9f8946c17bb2044e1478308
nnourr <nourqwerty123@gmail.com>
2025-03-18 14:38:40 -0400

    undo button

--- express/src/__tests__/__mocks__/redis.mock.ts
+++ express/src/__tests__/__mocks__/redis.mock.ts
@@ -3,36 +3,11 @@ import { jest } from "@jest/globals";
 export class mockRedis {
   private static store: Record<string, any[]> = {};
 
-  static flushAll = jest.fn(() => {
-    for (const key in this.store) {
-      delete this.store[key];
-    }
-    return Promise.resolve();
-  });
+  static flushAll = jest.fn();
 
-  static xAdd = jest.fn((key: string, id: string, payload: any) => {
-    if (!this.store[key]) {
-      this.store[key] = [];
-    }
-    // Use a fixed id for simplicity.
-    const entry = { id: "1-0", message: payload };
-    this.store[key].push(entry);
-    return Promise.resolve("1-0");
-  });
+  static xAdd = jest.fn();
 
-  static xRead = jest.fn(
-    (options: any, { key, id }: { key: string; id: string }) => {
-      if (this.store[key] && this.store[key].length > 0) {
-        return Promise.resolve([
-          {
-            name: Buffer.from(key),
-            messages: this.store[key],
-          },
-        ]);
-      }
-      return Promise.resolve(null);
-    }
-  );
+  static xRead = jest.fn();
 
-  static commandOptions = jest.fn((opts: any) => opts);
+  static commandOptions = jest.fn();
 }
