a38e482074f086dc86a9bb0c3086bea965bf41d9
nnourr <nourqwerty123@gmail.com>
2025-03-25 22:33:18 -0400

    basic impl

--- react/src/components/umlEditor.component.tsx
+++ react/src/components/umlEditor.component.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useContext, useEffect, useRef, useState } from "react";
 import { Editor, Monaco } from "@monaco-editor/react";
 import * as Y from "yjs";
 import { WebsocketProvider } from "y-websocket";
@@ -6,6 +6,7 @@ import { MonacoBinding } from "y-monaco";
 import { editor } from "monaco-editor";
 import { DocumentModel } from "../models/document.model";
 import { IPlantUmlError } from "../models/plantUmlError.model";
+import { UserContext } from "./user.context";
 
 const serverWsUrl =
   import.meta.env.VITE_SERVER_WS_URL || "http://localhost:3002";
@@ -17,7 +18,9 @@ interface UmlEditorProps {
   setEditorValue: (newVal: string) => void;
   error?: IPlantUmlError;
 }
+const colors = ["red", "green", "blue", "yellow", "purple", "orange", "cyan"];
 
+const color = colors[Math.floor(Math.random() * colors.length)];
 export const UmlEditor: React.FC<UmlEditorProps> = ({
   roomId,
   currDocument,
@@ -26,6 +29,7 @@ export const UmlEditor: React.FC<UmlEditorProps> = ({
   error,
 }) => {
   const [wsID, setWsID] = useState<string>(`${roomId}${currDocument.id}`);
+  const clientsRef = useRef<number[]>([]);
   const [decorations, setDecorations] =
     useState<editor.IEditorDecorationsCollection | null>(null);
   const editorRef = useRef<editor.IStandaloneCodeEditor | null>(null);
@@ -33,6 +37,7 @@ export const UmlEditor: React.FC<UmlEditorProps> = ({
   const providerRef = useRef<WebsocketProvider | null>(null);
   const docRef = useRef<Y.Doc | null>(null);
   const bindingRef = useRef<MonacoBinding | null>(null);
+  const userContext = useContext(UserContext);
 
   useEffect(() => {
     setWsID(`${roomId}${currDocument.id}`);
@@ -57,6 +62,30 @@ export const UmlEditor: React.FC<UmlEditorProps> = ({
     }
   }, [error?.line]);
 
+  const updateClientStyleSheets = (
+    statesArray: [number, { [x: string]: any }][]
+  ) => {
+    statesArray.forEach((state) => {
+      const clientId = state[0];
+      console.log(clientId);
+      if (state[1].user) {
+        const styleSheet = document.createElement("style");
+        styleSheet.innerText = `
+        .yRemoteSelectionHead-${clientId}{
+          border-left: 2px solid ${state[1].user.color} ;
+          position:relative;
+        }
+        .yRemoteSelection-${clientId}{
+          background-color: ${state[1].user.color} !important;
+          opacity: 0.5 !important;
+        }
+      `;
+        document.head.appendChild(styleSheet);
+        // console.log("the color is" + state[1].user.color);
+      }
+    });
+  };
+
   const setBinding = useCallback(() => {
     // Clean up the previous provider if it exists
     if (providerRef.current) {
@@ -73,6 +102,26 @@ export const UmlEditor: React.FC<UmlEditorProps> = ({
     docRef.current = doc;
     const provider = new WebsocketProvider(serverWsUrl, wsID, doc);
     providerRef.current = provider;
+
+    provider.awareness.setLocalStateField("user", {
+      name: userContext.context?.displayName || "guest",
+      color: color,
+    });
+
+    provider.awareness.on("change", () => {
+      const statesArray = Array.from(provider.awareness.getStates());
+      const newClients = statesArray.map((state) => state[0]);
+      console.log(clientsRef.current); // Use ref for logging
+      const clientsChanged =
+        !clientsRef.current ||
+        clientsRef.current.length !== newClients.length ||
+        !clientsRef.current.every((client) => newClients.includes(client));
+
+      if (clientsChanged) {
+        clientsRef.current = newClients; // Update the ref with new clients
+        updateClientStyleSheets(statesArray);
+      }
+    });
 
     // Bind Yjs doc to Monaco editor model
     const type = doc.getText("monaco");
