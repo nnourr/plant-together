791c9122a587effe27d1ce9ec1041cae31ef0513
nnourr <nourqwerty123@gmail.com>
2025-04-17 15:18:58 -0400

    landing page tweaks

--- react/src/components/footer.component.tsx
+++ react/src/components/footer.component.tsx
@@ -15,21 +15,21 @@ export const Footer: React.FC<FooterProps> = ({ className }) => {
         onClose={() => setShowModal(false)}
       />
       <footer
-        className={`${className} bg-black md:px-16 md:py-12 w-full text-white md:text-3xl font-bold flex justify-between md:flex-row flex-col px-8 py-9 text-xl gap-4`}
+        className={`${className} bg-slate-800 md:px-16 md:py-6 w-full md:text-2xl font-bold flex justify-between md:flex-row flex-col px-8 py-6 text-xl gap-4`}
       >
-        <a
-          href="https://github.com/nnourr/plant-together"
-          target="__blank"
-          className="w-full"
-        >
-          View Github
-        </a>
         <button
           onClick={() => setShowModal(true)}
-          className="text-left text-blue-500 text-nowrap cursor-pointer"
+          className="text-left w-full text-blue-500 text-nowrap cursor-pointer"
         >
           Meet the Team
         </button>
+        <a
+          href="https://github.com/nnourr/plant-together"
+          target="__blank"
+          className="w-full text-white text-left md:text-right "
+        >
+          View Github
+        </a>
       </footer>
     </>
   );
